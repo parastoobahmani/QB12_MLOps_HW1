9097f228a75c76442fa1515829637a18b0794d0c
parastoobahmani <50665624+parastoobahmani@users.noreply.github.com>
2026-06-01 22:41:18 +0330

    Delete hw1-1 directory

--- hw1-1/extract.py
+++
@@ -1,5 +0,0 @@
-
-def read_csv_checked(path):
-    pd.read_csv(path)
-
-read_csv_checked('data/raw/listings_sample.csv')
--- hw1-1/src/airbnb_ops/extract.py
+++
@@ -1,5 +0,0 @@
-
-import pandas as pd
-
-def read_csv_checked(path):
-    return pd.read_csv(path)
